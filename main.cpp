4621d3f3b7feb5332459b1c648502d2e71ef71e8
J-0-S-E <Kristian.lokkeberg@gmail.com>
2024-11-03 22:19:25 +0100

    Tester ut flyging

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <threepp/threepp.hpp>
 #include <iostream>
 #include <thread>
+
 using namespace threepp;
 using namespace std;
 int main()
@@ -11,23 +12,36 @@ int main()
 
     // Opprett kamera
     PerspectiveCamera camera(60, canvas.aspect(), 0.1f, 1000.0f);
-    camera.position.set(3, 3, 3);
+    camera.position.set(0, 0, 2);
     camera.lookAt(Vector3(0, 0, 0));
 
     Scene scene;
-    scene.background = Color::blue;
+    scene.background = Color::lightskyblue;
 
-    // Opprett firkant
-    auto geometry = BoxGeometry::create(1,1,1);
+    // Last inn PNG
+    TextureLoader texture_loader;
+    auto texture = texture_loader.load("C:/dev/Flappy Bird/Textures/Flappy Bird.png");
+    auto geometry = PlaneGeometry::create(1,1);
     auto material = MeshBasicMaterial::create();
-    material->color = Color::aliceblue;
+    material->map = texture;
+    material->transparent = true;
     auto square = Mesh::create(geometry,material);
     scene.add(square);
 
+    float gravity = -0.001f;
+    float velocity = 0.02f; // Fuglens hastighet
+    bool is_flapping = false;
 
     canvas.animate([&]() {
+        if (square->position.y > 1.5f || square->position.y < -1.5f) {
+            velocity = -velocity;
+        }
+
+        square->position.y += velocity;
+
         renderer.render(scene, camera);
     });
 
     return 0;
 }
+
